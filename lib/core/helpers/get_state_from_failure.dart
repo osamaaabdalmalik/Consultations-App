767abc7de8d42b3bd983962072e6556307d11822
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-05-08 11:50:47 +0300

    ♻️ show messages of failure in `get_state_from_failure`
    ✅ Done

--- lib/core/helpers/get_state_from_failure.dart
+++ lib/core/helpers/get_state_from_failure.dart
@@ -1,34 +1,43 @@
 import 'package:consultations_app/core/constants/app_strings.dart';
 import 'package:consultations_app/core/enums/general_states.dart';
 import 'package:consultations_app/core/helpers/failures.dart';
-import 'package:consultations_app/core/services/easy_loader_service.dart';
+import 'package:consultations_app/core/services/status_handler_service.dart';
 
 GeneralStates getStateFromFailure(Failure failure) {
   if (failure is OfflineFailure) {
+    StatusHandlerService.showOfflineError();
     return GeneralStates.offline;
-  } else if (failure is UnAuthenticatedFailure) {
-    EasyLoaderService.showError(
+  }
+  if (failure is BadRequestFailure) {
+    StatusHandlerService.showError(message: failure.message);
+    return GeneralStates.badRequest;
+  }
+  if (failure is UnAuthenticatedFailure) {
+    StatusHandlerService.showError(
       message: AppStrings.forbidden,
-      durationSeconds: 5,
+      durationSeconds: 8,
     );
     return GeneralStates.forbidden;
-  } else if (failure is UnAuthorizedFailure) {
-    EasyLoaderService.showError(
+  }
+  if (failure is UnAuthorizedFailure) {
+    StatusHandlerService.showError(
       message: AppStrings.unauthorized,
-      durationSeconds: 5,
+      durationSeconds: 8,
     );
     return GeneralStates.unAuthorized;
-  } else if (failure is NotFoundFailure) {
-    EasyLoaderService.showError(
+  }
+  if (failure is NotFoundFailure) {
+    StatusHandlerService.showError(
       message: AppStrings.notFound,
-      durationSeconds: 5,
     );
     return GeneralStates.notFound;
-  } else if (failure is BadRequestFailure) {
-    return GeneralStates.badRequest;
-  } else if (failure is InternalServerErrorFailure) {
-    return GeneralStates.internalServerProblem;
-  } else {
-    return GeneralStates.unexpectedProblem;
   }
+  if (failure is InternalServerErrorFailure) {
+    StatusHandlerService.showInternalServerError();
+    return GeneralStates.internalServerProblem;
+  }
+  StatusHandlerService.showError(
+    message: AppStrings.unexpectedException,
+  );
+  return GeneralStates.unexpectedProblem;
 }
