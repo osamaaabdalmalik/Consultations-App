006d1f2a63907f2ec1a927cd3a24134f8d7b4133
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-05-10 15:38:47 +0300

    ♻️ refactor order element in `home_screen` to improve `loading operation`
    ✅ Done

--- lib/features/main/presentation/screens/home_screen.dart
+++ lib/features/main/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:consultations_app/core/constants/app_keys.dart';
 import 'package:consultations_app/core/constants/app_routes.dart';
+import 'package:consultations_app/core/enums/experts_types.dart';
 import 'package:consultations_app/core/widgets/primary_loader.dart';
 import 'package:consultations_app/features/main/domain/entities/home_data_entity.dart';
 import 'package:consultations_app/features/main/presentation/cubits/main_cubit/main_cubit.dart';
@@ -9,6 +11,7 @@ import 'package:consultations_app/features/main/presentation/widgets/specialists
 import 'package:consultations_app/features/main/presentation/widgets/top_experts_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,24 +19,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<MainCubit, MainState>(
-      builder: (context, state) {
-        return state.maybeWhen(
-          loading: () => const Center(
-            child: PrimaryLoader(),
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        const HomeAppbar(),
+        SliverList(
+          delegate: SliverChildListDelegate.fixed(
+            [
+              BlocBuilder<MainCubit, MainState>(
+                builder: (context, state) {
+                  return state.maybeWhen(
+                    loading: () => Center(
+                      child: PrimaryLoader(
+                        padding: EdgeInsets.symmetric(vertical: 250.h),
+                      ),
+                    ),
+                    orElse: () => const SizedBox(),
+                    loaded: (homeData) => HomeContent(homeData: homeData),
+                    changeTabSuccess: (homeData) {
+                      if (homeData != null) {
+                        return HomeContent(homeData: homeData);
+                      }
+                      return const Center(
+                        child: PrimaryLoader(),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
           ),
-          orElse: () => const SizedBox(),
-          loaded: (homeData) => HomeContent(homeData: homeData),
-          changeTabSuccess: (homeData) {
-            if (homeData != null) {
-              return HomeContent(homeData: homeData);
-            }
-            return const Center(
-              child: PrimaryLoader(),
-            );
-          },
-        );
-      },
+        )
+      ],
     );
   }
 }
@@ -45,35 +62,38 @@ class HomeContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: [
-        const HomeAppbar(),
-        SliverList(
-          delegate: SliverChildListDelegate.fixed(
-            [
-              const HeaderSection(
-                title: "Specialists",
-              ),
-              SpecialistsList(mainCategories: homeData.mainCategories),
-              HeaderSection(
-                title: "Top Experts",
-                onSeeAllTap: () {
-                  context.push(AppRoutes.categoryExpertsScreen);
-                },
-              ),
-              TopExpertsList(
-                experts: homeData.highestRatedExperts,
-              ),
-              HeaderSection(
-                title: "Recommended Experts",
-                onSeeAllTap: () {
-                  context.push(AppRoutes.categoryExpertsScreen);
-                },
-              ),
-              RecommendedExpertsList(experts: homeData.highestRecommendedExperts),
-            ],
-          ),
+    return Column(
+      children: [
+        const HeaderSection(
+          title: "Specialists",
         ),
+        SpecialistsList(mainCategories: homeData.mainCategories),
+        HeaderSection(
+          title: "Top Experts",
+          onSeeAllTap: () {
+            context.push(
+              AppRoutes.expertsScreen,
+              extra: {
+                AppKeys.expertsType: ExpertsTypes.topExperts,
+              },
+            );
+          },
+        ),
+        TopExpertsList(
+          experts: homeData.highestRatedExperts,
+        ),
+        HeaderSection(
+          title: "Recommended Experts",
+          onSeeAllTap: () {
+            context.push(
+              AppRoutes.expertsScreen,
+              extra: {
+                AppKeys.expertsType: ExpertsTypes.recommendedExperts,
+              },
+            );
+          },
+        ),
+        RecommendedExpertsList(experts: homeData.highestRecommendedExperts),
       ],
     );
   }
