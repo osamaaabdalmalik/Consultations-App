20fc8361da6fe5473d02a9321bcf23f434decce0
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-05-10 15:36:39 +0300

    ♻️ add `padding` attribute to `primary_loader`
    ✅ Done

--- lib/core/widgets/primary_loader.dart
+++ lib/core/widgets/primary_loader.dart
@@ -5,18 +5,23 @@ import 'package:loading_animation_widget/loading_animation_widget.dart';
 class PrimaryLoader extends StatelessWidget {
   final double size;
   final Color color;
+  final EdgeInsetsGeometry padding;
 
   const PrimaryLoader({
     super.key,
     this.size = 50,
     this.color = AppColors.primary,
+    this.padding = EdgeInsets.zero,
   });
 
   @override
   Widget build(BuildContext context) {
-    return LoadingAnimationWidget.threeArchedCircle(
-      size: size,
-      color: color,
+    return Padding(
+      padding: padding,
+      child: LoadingAnimationWidget.threeArchedCircle(
+        size: size,
+        color: color,
+      ),
     );
   }
 }
