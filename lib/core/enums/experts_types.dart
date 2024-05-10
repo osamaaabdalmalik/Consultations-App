1ecb056efab36882d8abecac3b3ef2500bf09bb1
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-05-10 09:53:46 +0300

    ♻️ create `experts_types` enum to detect experts type that come from backend
    ✅ Done

---
+++ lib/core/enums/experts_types.dart
@@ -0,0 +1,9 @@
+enum ExpertsTypes {
+  allExperts('all_experts'),
+  topExperts('top_experts'),
+  recommendedExperts('recommended_experts');
+
+  const ExpertsTypes(this.value);
+
+  final String value;
+}
