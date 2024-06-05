c2a811709d71dbe3e3abdce4ae3b72505676e2f1
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-06-05 17:27:19 +0300

    ♻️ add padding and borderRadius for `primary_icon_button`
    ✅ Done

--- lib/core/widgets/primary_icon_button.dart
+++ lib/core/widgets/primary_icon_button.dart
@@ -5,23 +5,39 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class PrimaryIconButton extends StatelessWidget {
   final Widget icon;
   final Color color;
+  final EdgeInsetsGeometry? padding;
+  final BorderRadius? borderRadius;
   final void Function()? onPressed;
 
   const PrimaryIconButton({
     required this.icon,
-    this.color = AppColors.primary,
+    this.color = AppColors.primary20,
     this.onPressed,
     super.key,
+    this.padding,
+    this.borderRadius,
   });
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onPressed,
-      borderRadius: BorderRadius.circular(5.r),
-      child: Padding(
-        padding: EdgeInsets.all(10.h),
-        child: icon,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: borderRadius ?? BorderRadius.circular(5.r),
+        color: color,
+      ),
+      child: Material(
+        color: AppColors.transparent,
+        child: InkWell(
+          onTap: onPressed,
+          borderRadius: borderRadius ?? BorderRadius.circular(5.r),
+          child: Container(
+            padding: padding ?? EdgeInsets.all(10.h),
+            decoration: BoxDecoration(
+              borderRadius: borderRadius ?? BorderRadius.circular(5.r),
+            ),
+            child: icon,
+          ),
+        ),
       ),
     );
   }
