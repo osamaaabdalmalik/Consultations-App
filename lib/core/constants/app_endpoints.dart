a1bb1680a397a1279f13fddcf3af1620add1a84e
Osama Abd Al Malik <osamaaabdalmalik@gmail.com>
2024-06-20 11:39:57 +0300

    ♻️ change base url to run on A34
    ✅ Done

--- lib/core/constants/app_endpoints.dart
+++ lib/core/constants/app_endpoints.dart
@@ -1,6 +1,6 @@
 abstract class AppEndpoints {
   // static const String baseUrl = "10.0.2.2:8000"; // For Emulator
-  static const String baseUrl = "192.168.1.113:8000"; // For A34
+  static const String baseUrl = "192.168.163.247:8000"; // For A34
   static const String apiVersion = "/api/v1"; // For Emulator
 
   static const String getCategoriesAsPair = "/category/get_categories_as_pair";
